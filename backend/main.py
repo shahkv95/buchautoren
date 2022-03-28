68642cef630c47f8fde51946c100b40602519164
shahkv95 <kushshah253@gmail.com>
2022-03-29 02:41:02 +0530

    API requests: added get, post put and delete and checked the output at localhsot:8000/docs

--- backend/main.py
+++ backend/main.py
@@ -17,3 +17,23 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"Ping": "Pong"}
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+@app.get("/api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+
+@app.post("/api/todo")
+async def post_todo(todo):
+    return 1
+
+@app.put("/api/todo{id}")
+async def put_todo(id, data):
+    return 1
+
+@app.delete("/api/todo{id}")
+async def delete_todo(id):
+    return 1

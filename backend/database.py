6454cfd0911c670065126734d8ca42762476fd50
shahkv95 <kushshah253@gmail.com>
2022-03-29 23:45:03 +0530

    Added functions for applying CRUD operations in database.py

--- backend/database.py
+++ backend/database.py
@@ -5,3 +5,30 @@ import motor.motor_asyncio
 
 client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://cluster0.g4amf.mongodb.net/myFirstDatabase?appName=mongosh+1.3.1')
 
+database = client.TodoList
+collection = database.todo
+
+async def fetch_one_todo(title):
+    document = await collection.find_one({"title": title})
+    return document
+
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+    async for document in cursor:
+        todos.append(Todo(**document))
+    return todos
+
+async def create_todo(todo):
+    document = todo
+    result = await collection.insert_one(document)
+    return result
+
+async def update_todo(title, description):
+    await collection.update_one({"title": title}, {"$set":{"description": description}})
+    document = await collection.find_one({"title":title})
+    return document
+
+async def remove_todo(title):
+    await collection.delete_one({"title": title})
+    return True

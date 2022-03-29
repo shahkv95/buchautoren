62920ba9a5af623478f32cb94e77106500d1b218
shahkv95 <kushshah253@gmail.com>
2022-03-30 04:36:14 +0530

    minor fix for unused variable

--- backend/database.py
+++ backend/database.py
@@ -32,7 +32,7 @@ async def fetch_all_todos():
 
 async def create_todo(todo):
     document = todo
-    result = await collection.insert_one(document)
+    await collection.insert_one(document)
     return document
 
 

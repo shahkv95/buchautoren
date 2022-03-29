ee49661684d937ab6c00bef870b1a36ef313c768
shahkv95 <kushshah253@gmail.com>
2022-03-29 23:33:41 +0530

    Setup mongodb driver for connecting backend with mongodb server hosted on gcp cloud

---
+++ backend/database.py
@@ -0,0 +1,7 @@
+from model import Todo
+
+# MongoDB driver
+import motor.motor_asyncio
+
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://cluster0.g4amf.mongodb.net/myFirstDatabase?appName=mongosh+1.3.1')
+

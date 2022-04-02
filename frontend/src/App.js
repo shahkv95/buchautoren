8a0bf9757fda936fee6d64252b727c60a321b7be
shahkv95 <kushshah253@gmail.com>
2022-04-02 16:32:22 +0530

    Created the landing page for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div className="App">
-      Hello, World!
-      FARM Stack!
-    </div>
+      <div className= "App list-group-item justify-content-center align-items-center mx-auto" style={{"width": "400px", "backgroundColor": "white", "marginTop": "15px"}}>
+
+        <h1 className= "card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
+
+        <h6 className= "card text-white bg-primary mb-3">FARM Stack</h6>
+
+        <div className= "card-body">
+
+          <h5 className= "card text-white bg-dark mb-3">Add your task</h5>
+
+          <span className= "card-text">
+
+            <input className= "mb-2 form-control titleIn" placeholder='Title'/>
+            <input className= "mb-2 form-control descIn" placeholder='Description'/>
+            
+            <button className= "btn btn-outline-primary mx-2 mb-3" style={{"font-weight": "bold"}}>Add Task</button>
+
+          </span>
+
+          <h5 className= "card text-white bg-dark mb-3">Your Tasks</h5>
+
+          <div>
+            {/* Todo items - external component */}
+          </div>
+
+        </div>
+
+        <h6 className= "card text-dark bg-warning mb-0 py-1">Copyright 2022, All rights reserved &copy;</h6> 
+
+      </div>
   );
 }
 

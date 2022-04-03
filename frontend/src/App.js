17aab66bd5438a527a6dddf77538467c6087898d
shahkv95 <kushshah253@gmail.com>
2022-04-03 17:33:12 +0530

    updated the semantics in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,33 +30,33 @@ function App() {
   return (
       <div className= "App list-group-item justify-content-center align-items-center mx-auto" style={{"width": "400px", "backgroundColor": "white", "marginTop": "15px"}}>
 
-        <h1 className= "card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
+        <h1 className= "card text-white bg-primary mb-1" styleName="max-width: 20rem;">Buchautoren</h1>
 
-        <h6 className= "card text-white bg-primary mb-3">FARM Stack</h6>
+        <h6 className= "card text-white bg-primary mb-3 pt-1 pb-1">App for creating a list of books and their authors</h6>
 
         <div className= "card-body">
 
-          <h5 className= "card text-white bg-dark mb-3">Add your task</h5>
+          <h5 className= "card text-white bg-dark mb-3  pt-1 pb-1">Add your book list</h5>
 
           <form className= "card-text">
 
-            <input className= "mb-2 form-control titleIn" placeholder='Title' onChange={event => setTitle(event.target.value)} value={title} required/>
+            <input className= "mb-2 form-control titleIn" placeholder='Book Title' onChange={event => setTitle(event.target.value)} value={title} required/>
 
-            <input className= "mb-2 form-control descIn" placeholder='Description' onChange={event => setDesc(event.target.value)} ref={descInput} disabled={title === ""}/>
+            <input className= "mb-2 form-control descIn" placeholder='Author names' onChange={event => setDesc(event.target.value)} ref={descInput} disabled={title === ""}/>
 
-            <button className= "btn btn-outline-primary mx-2 mb-3" style={{"font-weight": "bold"}} onClick={addTodoHandler} disabled={title === ""}>Add Task</button>
+            <button className= "btn btn-outline-primary mx-2 mb-3" style={{"font-weight": "bold"}} onClick={addTodoHandler} disabled={title === ""}>Add Details</button>
 
             <button className= "btn btn-outline-primary mx-2 mb-3" style={{"font-weight": "bold"}} onClick={clearInput} disabled={desc === ""}>Reset Form</button>
 
           </form>
 
-          <h5 className= "card text-white bg-dark mb-3">Your Tasks</h5>
+          <h5 className= "card text-white bg-dark mb-3 pt-1 pb-1">Your Books and Authors</h5>
 
           <div>
             <TodoView todoList={todoList}/>
           </div>
         </div>
-        <h6 className= "card text-dark bg-warning mb-0 py-1">Copyright 2022, All rights reserved &copy;</h6> 
+        <h6 className= "card text-dark bg-warning mb-0 py-1">Made with FARM Stack: FastAPI - React - MongoDB</h6>
       </div>
   );
 }
